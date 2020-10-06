0537533b4f414d7049b4b0f09667a2781920d61a
Irvin Fiz <irvin.fiz.dev@gmail.com>
2020-10-06 15:31:00 -0500

    fix filter

--- src/config/index.js
+++ src/config/index.js
@@ -1,24 +1,24 @@
-const url_image = 'https://hr-app-server.irvinfiz.now.sh/image-upload';
+const url_image = "https://hr-app-server.irvinfiz.now.sh/image-upload";
 const opts_image = {
-    method: "POST"
+  method: "POST",
 };
-                      
-const hr_server = 'https://hr-app-server.adrian-injection.vercel.app/graph';
+
+const hr_server = "https://hr-app-server.adrian-injection.vercel.app/graph";
 // const hr_server = 'https://hr-app-server.irvinfiz.now.sh/graph';
 // const hr_server = 'http://localhost:4001/graph';
 
-const url = 'https://injection.adrian-injection.vercel.app/injection';
+const url = "https://injection.adrian-injection.vercel.app/injection";
 // const url = 'https://injection.irvinfiz.now.sh/injection'
 // const url = 'http://localhost:4000/injection'
 
 const opts = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" }
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
 };
 
 const hr_opts = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" }
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
 };
 
-export { url, opts, url_image, opts_image, hr_server, hr_opts }
+export { url, opts, url_image, opts_image, hr_server, hr_opts };
--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,140 +1,137 @@
-import React,{ useEffect } from 'react'
-import { connect } from 'react-redux'
-import { fetchMoldeCleanings } from '../Cleanings/actions'
-import { fetchSelectedCycles } from './actions'
-import Spinner from '../../components/Spinner'
-import './calendar.css'
-import MoldeCalendar from './MoldeCalendar'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchMoldeCleanings } from "../Cleanings/actions";
+import { fetchSelectedCycles } from "./actions";
+import Spinner from "../../components/Spinner";
+import "./calendar.css";
+import MoldeCalendar from "./MoldeCalendar";
 
+const date = new Date();
+const year = date.getFullYear();
 
-const date = new Date()
-const year = date.getFullYear()
-
-const Calendar =  ({fetchMoldeCleanings, fetchSelectedCycles, cleanings, loadingCleanings, selectedCycles, loadingSelectedCycles}) =>{
-    
-    useEffect(() =>{
-        if(cleanings.length === 0 ){ 
-            fetchMoldeCleanings()
-        }
-    },[cleanings, fetchMoldeCleanings])
-
-    useEffect(() =>{
-        if(!selectedCycles){ 
-            
-            fetchSelectedCycles()
-        }
-    },[selectedCycles,fetchSelectedCycles])
-
-    
-    const getMonth = () =>{
-        const calendar =[
-            {   month: 'January',
-                days: 30,
-                m: '01' 
-            },
-            {   month: 'February',
-                days: 28,
-                m: '02'
-            },
-            {   month: 'March',
-                days: 31,
-                m: '03'
-            },
-            {   month: 'April',
-                days: 30,
-                m: '04'
-            },
-            {   month: 'May',
-                days: 31,
-                m: '05'
-            },
-            {   month: 'June',
-                days: 30,
-                m: '06'
-            },
-            {   month: 'July',
-                days: 31,
-                m: '07'
-            },
-            {   month: 'August',
-                days: 31,
-                m: '08'
-            },
-            {   month: 'September',
-                days: 30,
-                m: '09'
-            },
-            {   month: 'October',
-                days: 31,
-                m: '10'
-            },
-            {   month: 'November',
-                days: 30,
-                m: '11'
-            },
-            {   month: 'December',
-                days: 31,
-                m: '12'
-            }
-        ]
-        const date = new Date()
-        const month = date.getMonth()
-        return calendar[month]
+const Calendar = ({
+  fetchMoldeCleanings,
+  fetchSelectedCycles,
+  cleanings,
+  loadingCleanings,
+  selectedCycles,
+  loadingSelectedCycles,
+}) => {
+  useEffect(() => {
+    if (cleanings.length === 0) {
+      fetchMoldeCleanings();
     }
+  }, [cleanings, fetchMoldeCleanings]);
 
-    const renderDays = ( ) =>{
-        let i;
-        let array = []
-        for (i = 1; i <= getMonth().days; i++) {
-            array = [...array, <th key={i}>{i}</th>]
-        }
-        return array
+  useEffect(() => {
+    if (!selectedCycles) {
+      fetchSelectedCycles();
     }
+  }, [selectedCycles, fetchSelectedCycles]);
 
-    const renderMoldeCalendar = (arr)=>{
-        if(arr.length === 0 ){ return null }
-        else {
-            return arr.map( (cleaning) =>{
-                const {_id, molde } = cleaning
-                if(!selectedCycles){
-                    return <MoldeCalendar key={_id} molde={molde} selected={null} days={getMonth().days}/>
-                }
-                else{
-                    const selected = selectedCycles.filter( cyc => cyc.molde === molde._id )
-                    return <MoldeCalendar key={_id} molde={molde} selected={selected} year={year} month={getMonth().m} days={getMonth().days}/>
+  const getMonth = () => {
+    const calendar = [
+      { month: "January", days: 30, m: "01" },
+      { month: "February", days: 28, m: "02" },
+      { month: "March", days: 31, m: "03" },
+      { month: "April", days: 30, m: "04" },
+      { month: "May", days: 31, m: "05" },
+      { month: "June", days: 30, m: "06" },
+      { month: "July", days: 31, m: "07" },
+      { month: "August", days: 31, m: "08" },
+      { month: "September", days: 30, m: "09" },
+      { month: "October", days: 31, m: "10" },
+      { month: "November", days: 30, m: "11" },
+      { month: "December", days: 31, m: "12" },
+    ];
+    const date = new Date();
+    const month = date.getMonth();
+    return calendar[month];
+  };
 
-                }
-            })
-        }
+  const renderDays = () => {
+    let i;
+    let array = [];
+    for (i = 1; i <= getMonth().days; i++) {
+      array = [...array, <th key={i}>{i}</th>];
     }
+    return array;
+  };
 
+  const renderMoldeCalendar = (arr) => {
+    if (arr.length === 0) {
+      return null;
+    } else {
+      return arr.map((cleaning) => {
+        const { _id, molde } = cleaning;
+        if (!selectedCycles) {
+          return (
+            <MoldeCalendar
+              key={_id}
+              molde={molde}
+              selected={null}
+              days={getMonth().days}
+            />
+          );
+        } else {
+          const selected = selectedCycles.filter(
+            (cyc) => cyc.molde === molde._id
+          );
+          return (
+            <MoldeCalendar
+              key={_id}
+              molde={molde}
+              selected={selected}
+              year={year}
+              month={getMonth().m}
+              days={getMonth().days}
+            />
+          );
+        }
+      });
+    }
+  };
 
-    return(
-        loadingCleanings ? <Spinner></Spinner> :
-        <div className='calendar-container'>
-            <table className='calendar-table'>
-                <thead>
-                    <tr>
-                        <th className='month-calendar' colSpan={getMonth().days+1}>{getMonth().month}</th>
-                    </tr>
-                    <tr>
-                       <th>Mold</th>
-                       {renderDays()} 
-                    </tr>                    
-                </thead>
-                {!loadingSelectedCycles && renderMoldeCalendar(cleanings.filter(item => item.active === true && item.date >= `${year}-${getMonth().m}-01` && item.date <= `${year}-${getMonth().m}-31` ))}
-            </table>
-        </div>
-    )
-}
+  return loadingCleanings ? (
+    <Spinner></Spinner>
+  ) : (
+    <div className="calendar-container">
+      <table className="calendar-table">
+        <thead>
+          <tr>
+            <th className="month-calendar" colSpan={getMonth().days + 1}>
+              {getMonth().month}
+            </th>
+          </tr>
+          <tr>
+            <th>Mold</th>
+            {renderDays()}
+          </tr>
+        </thead>
+        {!loadingSelectedCycles &&
+          renderMoldeCalendar(
+            cleanings.filter(
+              (item) => item.active === true
+              //     &&
+              // item.date >= `${year}-${getMonth().m}-01` &&
+              // item.date <= `${year}-${getMonth().m}-31`
+            )
+          )}
+      </table>
+    </div>
+  );
+};
 
-const mapStateToProps = state =>({
-    moldes: state.moldes,
-    cleanings: state.cleanings,
-    loadingCleanings: state.loadingCleanings,
-    selectedCycles: state.selectedCycles,
-    selectedCyclesMessage: state.selectedCyclesMessage,
-    loadingSelectedCycles: state.loadingSelectedCycles
-})
+const mapStateToProps = (state) => ({
+  moldes: state.moldes,
+  cleanings: state.cleanings,
+  loadingCleanings: state.loadingCleanings,
+  selectedCycles: state.selectedCycles,
+  selectedCyclesMessage: state.selectedCyclesMessage,
+  loadingSelectedCycles: state.loadingSelectedCycles,
+});
 
-export default connect(mapStateToProps,{fetchMoldeCleanings, fetchSelectedCycles })(Calendar)
+export default connect(mapStateToProps, {
+  fetchMoldeCleanings,
+  fetchSelectedCycles,
+})(Calendar);
